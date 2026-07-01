0c352dadfef450f1bce14b10312064eb8301d488
ayeshatahsin11 <ayeshatahsin1000@gmail.com>
2026-07-01 20:46:06 +0600

    header added

---
+++ src/app/(root)/Layout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const mainLayout = ({children}) => {
+  return (
+   <>
+   {children}
+   </>
+  )
+}
+
+export default mainLayout
---
+++ src/app/(root)/page.jsx
@@ -0,0 +1,17 @@
+import Footer from "@/components/layout/footer/Footer";
+import Header from "@/components/layout/header/Header";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <Header />
+      <div>page</div>
+      <Footer />
+    </>
+  );
+};
+
+export default page;
+
+// ekhane jei components gulo common rakhar ogulo place krbo
