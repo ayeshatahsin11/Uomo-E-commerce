19cc3a080d7c74f00603c1189f4a6da58ee17c2a
ayeshatahsin11 <ayeshatahsin1000@gmail.com>
2026-07-04 21:00:40 +0600

    stars rating added

--- src/components/common/ProductCards.jsx
+++ src/components/common/ProductCards.jsx
@@ -1,6 +1,9 @@
+"use client"
+
 import Image from "next/image";
 import React from "react";
 import ProductImage from "@/app/assests/Images/productImage01.jpg";
+import { Rating } from 'react-simple-star-rating'
 const ProductCards = () => {
   return (
     <>
@@ -8,6 +11,9 @@ const ProductCards = () => {
       <div>
         <h4>Makeup</h4>
         <h2>SheGlam Blush(Shade-blossom)</h2>
+    <div className="flex items-center gap-0.5">
+  <Rating initialValue={1} readonly size={20} />
+</div>
       </div>
     </>
   );
