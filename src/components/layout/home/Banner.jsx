2ec4e9b75bbf2bdc0ea90cb6440a59c13bb0ba40
ayeshatahsin11 <ayeshatahsin1000@gmail.com>
2026-07-04 10:01:54 +0600

    banner added

--- src/components/layout/home/Banner.jsx
+++ src/components/layout/home/Banner.jsx
@@ -1,17 +1,40 @@
 import React from "react";
 import Container from "../Container";
 import BannerImg from "../../../app/assests/Images/BannerImage.png";
+import BannerItem from "../../../app/assests/Images/bannerItem-image.png";
+import Link from "next/link";
+
+import Image from "next/image";
 const Banner = () => {
   return (
     <>
-      <section style={{ background: `url(${BannerImg.src})`  }} className="h-100">
+      <section
+        style={{ background: `url(${BannerImg.src})` }}
+        className="h-auto"
+      >
         <Container>
+          <div className="flex justify-between pt-[124px] pb-[107px]">
             <div>
-                <div>
-                    <h5>shop our freshest</h5>
-                    <h2>Fresh Hand-picked Vegetable Everyday</h2>
-                </div>
+              <h5 className=" pl-13.25 relative font-medium text-sm text-primary leading-6 uppercase after::content-[''] after:w-10 after:h-0.5 after:bg-primary after:absolute after:top-2/4 after:translate-y-[-50%] after:left-0">
+                shop our freshest
+              </h5>
+              <h2 className="font-bold text-secondary text-[50px] leading-15 mt-2.75">
+                Fresh Hand-Picked Vegetables{" "}
+                <span className="text-primary">Everyday</span>
+              </h2>
+              <Link href="#" className="font-medium mt-5 text-primary-black text-sm leading-6 cursor-pointer uppercase relative after:content-[''] after:w-0 hover:after:w-28 after:h-0.5 after:bg-primary-black after:absolute after:-bottom-1 after:left-0 after:duration-300">
+                Discover more
+              </Link>
             </div>
+            <div>
+              <Image
+                src={BannerItem}
+                alt="banner-item"
+                width={518}
+                height={329}
+              />
+            </div>
+          </div>
         </Container>
       </section>
     </>
