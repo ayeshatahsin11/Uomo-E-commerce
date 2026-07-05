865276bfd0c890a2ae4331e3d5d6793ba05ab7bb
ayeshatahsin11 <ayeshatahsin1000@gmail.com>
2026-07-05 21:16:53 +0600

    best sellers added

--- src/components/common/Nav&Tabs.jsx
+++ src/components/common/Nav&Tabs.jsx
@@ -6,36 +6,40 @@ const Nav_Tabs = () => {
   return (
     <>
       <Tabs defaultValue="overview">
-       <div className="absolute top-2.75 right-0">
-         <TabsList variant="line">
-          <TabsTrigger
-            className="text-primary-gray text-[16px] font-medium after:w-[50%] px-0  after:duration-300"
-            value="Best Sellers"
-          >
-            Best Sellers
-          </TabsTrigger>
-          <TabsTrigger
-            className="text-primary-gray text-[16px] font-medium after:w-[50%] px-0 ml-13.5 after:duration-300"
-            value="Most Popular"
-          >
-            Most Popular
-          </TabsTrigger>
-          <TabsTrigger
-            className="text-primary-gray text-[16px] font-medium after:w-[50%] px-0 ml-13.5 after:duration-300"
-            value="Top 20"
-          >
-            Top 20
-          </TabsTrigger>
-          <TabsTrigger
-            className="text-primary-gray text-[16px] font-medium after:w-[50%] px-0 ml-13.5 after:duration-300"
-            value="Best Rated"
-          >
-            Best Rated
-          </TabsTrigger>
-        </TabsList>
-       </div>
+        <div className="absolute top-2.75 right-0">
+          <TabsList variant="line">
+            <TabsTrigger
+              className="text-primary-gray text-[16px] font-medium after:w-[50%] px-0  after:duration-300"
+              value="Best Sellers"
+            >
+              Best Sellers
+            </TabsTrigger>
+            <TabsTrigger
+              className="text-primary-gray text-[16px] font-medium after:w-[50%] px-0 ml-13.5 after:duration-300"
+              value="Most Popular"
+            >
+              Most Popular
+            </TabsTrigger>
+            <TabsTrigger
+              className="text-primary-gray text-[16px] font-medium after:w-[50%] px-0 ml-13.5 after:duration-300"
+              value="Top 20"
+            >
+              Top 20
+            </TabsTrigger>
+            <TabsTrigger
+              className="text-primary-gray text-[16px] font-medium after:w-[50%] px-0 ml-13.5 after:duration-300"
+              value="Best Rated"
+            >
+              Best Rated
+            </TabsTrigger>
+          </TabsList>
+        </div>
         <TabsContent value="Best Sellers">
-       <ProductCards/>
+          <div className="grid grid-cols-5 ">
+            {Array.from({ length: 15 }).map((_, index) => (
+              <ProductCards />
+            ))}
+          </div>
         </TabsContent>
         <TabsContent value="Most Popular">
           <p>Here are the popular items</p>
