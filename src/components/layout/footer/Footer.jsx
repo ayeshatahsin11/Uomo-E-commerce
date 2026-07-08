bd5aed78d9705b477a350ee16a7e2ef4477b4550
ayeshatahsin11 <ayeshatahsin1000@gmail.com>
2026-07-08 22:00:06 +0600

    footer part ongoing

--- src/components/layout/footer/Footer.jsx
+++ src/components/layout/footer/Footer.jsx
@@ -1,21 +1,13 @@
 import React from "react";
 import Container from "../Container";
-import Logo from "@/components/common/Logo";
-import Socialicons from "./Socialicons";
+import Info from "./Info";
 
 const Footer = () => {
   return (
     <section className="bg-secondary text-white mb-25">
       <Container>
         <div id="main grid part">
-          <div className="text-sm leading-6 max-w-70.5">
-            <Logo/>
-            <h3 className="mt-11">1418 River Drive, Suite 35 Cottonhall, CA 9622
-United States</h3>
-<h4 className="mt-5">sale@uomo.com</h4>
-<h5>+1 246-345-0695</h5>
-<Socialicons/>
-          </div>
+          <Info />
         </div>
       </Container>
     </section>
@@ -23,3 +15,7 @@ United States</h3>
 };
 
 export default Footer;
+
+
+
+//ekta common design title banaba, then array baniye props pass krbo, see navbar and privilige
---
+++ src/components/layout/footer/Info.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Logo from "@/components/common/Logo";
+import Socialicons from "./Socialicons";
+const Info = () => {
+  return (
+    <div className="text-sm leading-6 max-w-70.5">
+            <Logo/>
+            <h3 className="mt-11">1418 River Drive, Suite 35 Cottonhall, CA 9622
+United States</h3>
+<h4 className="mt-5">sale@uomo.com</h4>
+<h5>+1 246-345-0695</h5>
+<Socialicons/>
+          </div>
+  )
+}
+
+export default Info
