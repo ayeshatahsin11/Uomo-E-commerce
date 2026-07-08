d9921cdf91a205868878cba42bb58e84a299f481
ayeshatahsin11 <ayeshatahsin1000@gmail.com>
2026-07-08 10:00:43 +0600

    blog part done

--- src/components/layout/Contents/BlogNews.jsx
+++ src/components/layout/Contents/BlogNews.jsx
@@ -1,44 +1,50 @@
-"use client"
-import { useState, useEffect } from 'react'
-import React from 'react'
-import Container from '../Container'
-import Image from 'next/image'
+"use client";
+import { useState, useEffect } from "react";
+import React from "react";
+import Container from "../Container";
+import Image from "next/image";
 
 const BlogNews = () => {
-      const [blog, setBlog] = useState([]);
-         useEffect(() => {
-        const fetchBlog = async () => {
-          const res = await fetch("/api/blog"); // lowercase, matches folder
-          const data = await res.json();
-          setBlog(data);
-        };
-        fetchBlog();
-      }, []);
-    
+  const [blog, setBlog] = useState([]);
+  useEffect(() => {
+    const fetchBlog = async () => {
+      const res = await fetch("/api/blog"); // lowercase, matches folder
+      const data = await res.json();
+      setBlog(data);
+    };
+    fetchBlog();
+  }, []);
+
   return (
     <>
-    <Container>
-         <div className='flex gap-7.5 mt-8.5 text-center'>
-           {blog?.map((item)=>(
-            <div key={item.id} className='group'>
-               <div className="relative w-82.5 h-55 overflow-hidden">
-              <Image
-                src={item?.image}
-                alt={item.about}
-                fill
-                sizes="330px"
-                className="object-contain cursor-pointer"
-              />
+      <Container>
+        <div className="flex gap-7.5 mt-8.5">
+          {blog?.map((item) => (
+            <div key={item.id} className="w-82.5 h-auto bg-white">
+              <div className="relative w-82.5 h-55 overflow-hidden">
+                <Image
+                  src={item?.image}
+                  alt={item.about}
+                  fill
+                  sizes="330px"
+                  className="object-contain cursor-pointer"
+                />
+              </div>
+              <div className="pt-6  pb-5 pl-7.75 pr-15">
+                <div className="flex gap-6.5 text-sm text-primary-gray leading-6 mt-6 uppercase">
+                  <h3>By {item?.author}</h3>
+                  <h4>{item?.date}</h4>
+                </div>
+                <h2 className="text-[18px] text-primary-black mt-1">
+                  {item?.about}
+                </h2>
+              </div>
             </div>
-            <h3>{item?.author}</h3>
-            <h4>{item?.date}</h4>
-            <h2>{item?.about}</h2>
-           </div>
-           ))}
+          ))}
         </div>
-    </Container>
+      </Container>
     </>
-  )
-}
+  );
+};
 
-export default BlogNews
+export default BlogNews;
