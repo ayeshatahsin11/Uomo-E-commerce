921fabb8ebaf256c07c2972e526b35fa44ee563b
ayeshatahsin11 <ayeshatahsin1000@gmail.com>
2026-07-11 21:06:34 +0600

    category res ongoing

--- src/components/common/CategoryCards.jsx
+++ src/components/common/CategoryCards.jsx
@@ -1,12 +1,12 @@
-"use client"
-import { useState, useEffect } from 'react'
-import React from 'react'
-import Container from '../layout/Container'
-import Image from 'next/image'
+"use client";
+import { useState, useEffect } from "react";
+import React from "react";
+import Container from "../layout/Container";
+import Image from "next/image";
 
 const CategoryCards = () => {
-    const [category, setCategory] = useState([]);
-     useEffect(() => {
+  const [category, setCategory] = useState([]);
+  useEffect(() => {
     const fetchCategory = async () => {
       const res = await fetch("/api/category"); // lowercase, matches folder
       const data = await res.json();
@@ -17,28 +17,31 @@ const CategoryCards = () => {
 
   return (
     <>
-    <Container>
-        <div className='flex gap-7 mt-10 text-center'>
-           {category?.map((item)=>(
-            <div key={item.id} className='group'>
-               <div className="relative w-65 h-55 overflow-hidden">
-              <Image
-                src={item?.image}
-                alt={item.name}
-                fill
-                sizes="260px"
-                className="object-contain cursor-pointer"
-              />
+      <Container>
+        <div className="grid grid-cols-2 md:grid-cols-5 gap-3 md:gap-7 md:mt-10 text-center">
+          {category?.map((item) => (
+            <div key={item.id} className="group">
+              <div className="relative md:w-65 h-55 overflow-hidden">
+                <Image
+                  src={item?.image}
+                  alt={item.name}
+                  fill
+                  sizes="260px"
+                  className="object-contain cursor-pointer"
+                />
+              </div>
+              <h3 className="font-medium text-[16px] text-primary-black md:mt-5">
+                {item.name}
+              </h3>
+              <h4 className="text-primary-gray text-sm opacity-0 group-hover:opacity-100 duration-200">
+                {item.quantity} Products
+              </h4>
             </div>
-            <h3 className='font-medium text-[16px] text-primary-black mt-5'>{item.name}</h3>
-            <h4 className='text-primary-gray text-sm opacity-0 group-hover:opacity-100 duration-200'>{item.quantity} Products</h4>
-            </div>
-           ))}
+          ))}
         </div>
-    </Container>
+      </Container>
     </>
-  )
-}
+  );
+};
 
-export default CategoryCards
-
+export default CategoryCards;
