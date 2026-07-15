87b433d69a9a46b271ace1e3b4b2d96c9f0fa0dd
ayeshatahsin11 <ayeshatahsin1000@gmail.com>
2026-07-15 19:30:30 +0600

    skeleton added

--- src/components/shop/ShopProducts.jsx
+++ src/components/shop/ShopProducts.jsx
@@ -2,6 +2,8 @@
 import React, { useState, useEffect } from "react";
 import ProductCards from "../common/ProductCards";
 import { useShopStore } from "@/store/useShopStore";
+import { Card, CardContent, CardHeader } from "@/components/ui/card"
+import { Skeleton } from "@/components/ui/skeleton"
 import Paginate from "./Paginate";
 
 const gridColsMap = {
@@ -14,12 +16,13 @@ const ShopProducts = () => {
   const [products, setProducts] = useState([]);
   const sortValue = useShopStore((state) => state.sortValue);
   const gridView = useShopStore((state) => state.gridView);
-
+const [loading, setLoading] = useState(true)
   useEffect(() => {
     const fetchProducts = async () => {
       const res = await fetch("/api/products");
       const data = await res.json();
       setProducts(data);
+      setLoading(false)
     };
     fetchProducts();
   }, []);
@@ -40,15 +43,36 @@ const ShopProducts = () => {
   });
 
   return (
-    <Paginate
-      items={sortedProducts}
-      itemsPerPage={8}
-      wrapperClassName={`grid ${gridColsMap[gridView]} gap-x-6 gap-y-5 mt-10`}
-      renderItem={(product) => (
-        <ProductCards key={product.id} product={product} />
-      )}
-    />
+    <div>
+
+{loading ? (
+  <div className="grid grid-cols-4 gap-5">
+  {Array.from({length:8}).map((_,index)=>(
+     <Card className="w-full max-w-xs">
+      <CardContent>
+        <Skeleton className="aspect-video w-full" />
+      </CardContent>
+      <CardHeader>
+        <Skeleton className="h-4 w-2/3" />
+        <Skeleton className="h-4 w-1/2" />
+      </CardHeader>
+    </Card>
+  ))}
+  </div>
+)
+:( <Paginate
+        items={sortedProducts}
+        itemsPerPage={8}
+        wrapperClassName={`grid ${gridColsMap[gridView]} gap-x-6 gap-y-5 mt-10`}
+        renderItem={(product) => (
+          <ProductCards key={product.id} product={product} />
+        )}
+      />)}
+
+     
+    </div>
   );
 };
 
 export default ShopProducts;
+
